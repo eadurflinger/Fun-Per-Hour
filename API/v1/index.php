7c9f7f26d0f7edb200087b814673c880f9a3bc80
Sam Chen <samuel88835@yahoo.com>
2017-10-07 12:34:27 -0400

    testing twilio

---
+++ API/v1/index.php
@@ -0,0 +1,20 @@
+<?php
+
+require "../../vendor/autoload.php";
+use Twilio\Rest\Client;
+
+$sid = "AC11e0c32f223e9f9424f5de8664c56d3b";
+$token = "d8419de55b30f7d7acb918a07973f0b8";
+$client = new Client($sid, $token);
+
+$client->messages->create(
+'+17163590387', [
+    'from' => '+17243085071',
+    'body' => 'TESTING TWILIO API'
+]);
+
+
+
+
+
+?>
