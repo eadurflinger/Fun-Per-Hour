b42c51c504a25b8024ebd960f3ce84182d6a72af
Sam Chen <samuel88835@yahoo.com>
2017-10-07 13:51:35 -0400

    just trying to make this work on the server...

--- API/v1/index.php
+++ API/v1/index.php
@@ -7,10 +7,10 @@ $sid = "AC11e0c32f223e9f9424f5de8664c56d3b";
 $token = "d8419de55b30f7d7acb918a07973f0b8";
 $client = new Client($sid, $token);
 
-$client->messages->create([
-    'To' => '+13305037056',
-    'From' => '+17243085071',
-    'Body' => '<<MESSAGE>>'
+$client->messages->create(
+'+13305037056', [
+    'from' => '+17243085071',
+    'body' => '<<MESSAGE>>'
 ]);
 
 
