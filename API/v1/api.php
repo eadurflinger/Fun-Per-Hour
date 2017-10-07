c443853580eed0624195f010e3c7fe45428d85f7
Sam Chen <samuel88835@yahoo.com>
2017-10-07 14:00:44 -0400

    removing unnecesary stuff

--- API/v1/api.php
+++ API/v1/api.php
@@ -1,23 +1,6 @@
 <?php
 /*URI: /pubfunc/params[0]/params[1]/...
-***Allowed pubfuncs: #note that (stuff) is stored in API class under $input
-* login($col, $val)
-* forgotPass($user)
-* newUser($col, $val)
-* access($table, $col, $val, $orderby)
-* accessAd($id,$table, $col, $val, $orderby)
-* dump($table, $col, $val, $orderby) //returns excel file?
-* toggleEntry() //start or stops entry
-* inProjects($either in or not --an actual param i guess)
-***COMPARE TABLE & COLUMN NAMES TO KNOWN NAMES BEFORE ALLOWING THEM TO BE USED TO AVOID MYSQL INJECTION
-***DONT ALLOW USER INPUT TO GO INTO WHERE/TABLE (other than known values)
-***no one has access to
-* sql($id,$table, $col, $val, $orderby, $where)
 *
-***within the token...
-* expiration date
-* authority
-* u_ID
 */
 require_once 'API.class.php';
 require_once 'user.class.php';
@@ -39,21 +22,7 @@ class mAPI extends API {
       $this->user = new User($this->token->uid);
     }
     $this->data = $this->input['data'];
-    //TODO make sure we've received all the $input $k=>$v that are necessary
-    //TODO figure out AUTHENTICATION here...
-    // $APIKey = new Models\APIKey();
-    // $User = new Models\User();
-    //
-    // if (!array_key_exists('apiKey', $this->request)) {
-    //     throw new Exception('No API Key provided');
-    // } else if (!$APIKey->verifyKey($this->request['apiKey'], $origin)) {
-    //     throw new Exception('Invalid API Key');
-    // } else if (array_key_exists('token', $this->request) &&
-    //      !$User->get('token', $this->request['token'])) {
-    //
-    //     throw new Exception('Invalid User Token');
-    // }
-    // $this->User = $User;
+
   }
   protected function buildResponse($res) {
     $out = ['sqlres' => $res];
