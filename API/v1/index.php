5e365e1c23b1536d69ebed7796db13db4153131e
Sam Chen <samuel88835@yahoo.com>
2017-10-07 13:31:48 -0400

    add api.class and very basic test of twilio api

--- API/v1/index.php
+++ API/v1/index.php
@@ -7,14 +7,11 @@ $sid = "AC11e0c32f223e9f9424f5de8664c56d3b";
 $token = "d8419de55b30f7d7acb918a07973f0b8";
 $client = new Client($sid, $token);
 
-$client->messages->create(
-'+17163590387', [
+$client->messages->create([
+    'to' => '+13305037056',
     'from' => '+17243085071',
-    'body' => 'TESTING TWILIO API'
+    'body' => '<<MESSAGE>>'
 ]);
-
-
-
 
 
 ?>
