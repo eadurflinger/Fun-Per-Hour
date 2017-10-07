6fc97d552b7a361a774a942ced05e627c7008e01
Sam Chen <samuel88835@yahoo.com>
2017-10-07 16:30:07 -0400

    try this now...

--- API/v1/reply_test.php
+++ API/v1/reply_test.php
@@ -9,6 +9,10 @@ header("content-type: text/xml");
         <?php
         echo "Message Received:".$_POST['Body'];
         echo "From: ".$_POST['From'];
+
+        $myfile = fopen("log.txt", "w");
+        fwrite($myfile, json_encode(getallheaders()));
+        fclose($myfile);
         ?>
     </Message>
 </Response>
