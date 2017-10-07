a382f0063f148775d109bc32acd1a964107979ad
Sam Chen <samuel88835@yahoo.com>
2017-10-07 15:07:47 -0400

    lol i'm dumb

--- API/v1/reply_test.php
+++ API/v1/reply_test.php
@@ -6,6 +6,6 @@ header("content-type: text/xml");
 <Response>
     <Message>
         This is a response.
-        <?php json_encode($_POST); ?>
+        <?php echo json_encode($_POST); ?>
     </Message>
 </Response>
