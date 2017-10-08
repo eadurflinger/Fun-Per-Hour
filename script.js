33d59a415f0a209b59add153d13dfe34cbaedda5
eadurflinger <eadurflinger@gmail.com>
2017-10-07 22:23:30 -0400

    edited script

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-'use strict';
-
-let dat = {
-  data: {
-    set: {
-        'username' : 'USERNAME',
-        'password' : 'PASSWORD',
-        'adminname' : "NAME",
-        'adminemail' : "EMAIL",
-    }
-  },
-  newRand: '0',
+var dat = {
+    data: {
+        set: {
+            'username' : 'USERNAME',
+            'password' : 'PASSWORD',
+            'adminname' : "NAME",
+            'adminemail' : "EMAIL",
+        }
+    },
+    newRand: '0',
 };
 
-let res;
 
-$.ajax({
-  type: 'POST',
-  url: 'http://fun-per-hour.herokuapp.com/API/v1/newUser',
-  contentType: 'application/json',
-  dataType: 'json',
-  data: JSON.stringify(dat),
-  success: (result,status,xhr) => {
-    console.log(result);
-    console.log(`${JSON.stringify(xhr)} ${status}`);
-  },
-  error: (xhr, status, error) => {
-    console.log('err');
-    console.log (`${JSON.stringify(xhr)} ${status}`);
-  }
+
+$( document ).ready(function() {
+    
+    $('#login_button').click(function(e){
+        e.preventDefault();
+        console.log('clicked');
+        $.ajax({
+          type: 'POST',
+          url: 'http://fun-per-hour.herokuapp.com/API/v1/index.php/newUser',
+          contentType: 'application/json',
+          dataType: 'json',
+          data: JSON.stringify(dat),
+          success: (result,status,xhr) => {
+            console.log(result);
+            console.log(`${JSON.stringify(xhr)} ${status}`);
+          },
+          error: (xhr, status, error) => {
+            console.log('err');
+            console.log (`${JSON.stringify(xhr)} ${status}`);
+          }
+        });
+    });
+    
 });
-
-console.log(res);
